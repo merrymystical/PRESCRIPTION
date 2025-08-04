54c0c2a1044f9239cde5d3ec459a762291c057c7
Maria Zahid <153418292+merrymystical@users.noreply.github.com>
2025-08-04 20:18:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ st.markdown(
     <style>
     .stApp {
         background-image: url("https://media.licdn.com/dms/image/v2/D4D22AQHdEsYAb7Lkhw/feedshare-shrink_800/feedshare-shrink_800/0/1705580533339?e=1756944000&v=beta&t=zuX1L2vYlddscLLBr4FSzSOp2n4ZW877Oh-grS0uSRc");
-        background-size: contain;
+        background-size: cover;
         background-repeat: no-repeat;
-        background-position: top center;
+        background-position: bottom left;
         background-attachment: fixed;
     }
     </style>
