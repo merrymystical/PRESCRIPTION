b6fd112d73f5cc40979f7bd7aa9ba43307fa298a
Maria Zahid <153418292+merrymystical@users.noreply.github.com>
2025-08-05 09:20:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 import streamlit as st
 from io import BytesIO
 from extract import extract_prescription_fields, generate_filled_card
+import base64
 
+def set_background(image_file):
+    with open(image_file, "rb") as f:
+        encoded = base64.b64encode(f.read()).decode()
+    css = f"""
+    <style>
+    .stApp {{
+        background-image: url("data:image/jpg;base64,{encoded}");
+        background-size: cover;
+        background-position: center;
+        background-attachment: fixed;
+    }}
+    </style>
+    """
+    st.markdown(css, unsafe_allow_html=True)
+    
 # App configuration
 st.set_page_config(page_title="Prescription → Card", layout="centered")
+set_background("my_bg.jpg")
 st.markdown(
     """
     <style>
