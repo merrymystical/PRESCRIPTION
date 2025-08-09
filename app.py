94a3e177aee83ac22a612f7c3feb946db24dd582
Maria Zahid <153418292+merrymystical@users.noreply.github.com>
2025-08-09 20:01:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -164,8 +164,11 @@ if not st.session_state.authenticated:
             info@alsalamahospital.com
             """
             #message = f"From: {FROM_EMAIL}\nTo: {addr}\nSubject: {subject}\n\n{body}"
-            for addr in emails:
-                smtp.sendmail(FROM_EMAIL, addr, message.encode("utf-8"))
+            with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as smtp:
+                smtp.starttls()
+                smtp.login(SMTP_USER, SMTP_PASS)
+                for addr in emails:
+                    smtp.sendmail(FROM_EMAIL, addr, message_html.encode("utf-8"))
             st.info("An OTP has been sent to your recovery email")
 # If they’ve requested a reset, show OTP & new-password fields:
     if st.session_state.password_reset_otp:
